f70e91fc4afd9f30fc0e5a6cd3fe5152f468b317
Konstantin <konstantintkachev92@gmail.com>
2020-09-28 00:26:43 +0300

    Учебный проект: холст

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,33 @@
 'use strict';
 
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 260;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 20;
+var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 15;
 var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
-var colorColumns = ['rgba(255, 0, 0, 1)', 'hsl(244, 90%, 19%)', 'hsl(240, 3%, 41%)', 'hsl(252, 4%, 24%)'];
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx) {
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
   renderCloud(
     ctx,
     CLOUD_X + GAP,
@@ -30,14 +41,23 @@ window.renderStatistics = function(ctx) {
     '#fff'
   );
   ctx.fillStyle = '#000';
-  ctx.fillText(
-    'Вы',
-    CLOUD_X + TEXT_WIDTH, 
-    CLOUD_HEIGHT - GAP
-  );
-  ctx.fillRect(
-    CLOUD_X + TEXT_WIDTH, 
-    TEXT_WIDTH,
-    colorColumns[0]
-  );
+
+  var maxTime = getMaxElement(times);
+  var colorColumns = ['rgba(255, 0, 0, 1)', 'hsl(244, 90%, 19%)', 'hsl(240, 3%, 41%)', 'hsl(252, 4%, 24%)'];
+
+  for (var i = 0; i < players.length && i < colorColumns.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(
+      players[i],
+      CLOUD_X + TEXT_WIDTH + (TEXT_WIDTH + TEXT_WIDTH) * i, 
+      CLOUD_HEIGHT - GAP
+    );
+    ctx.fillStyle = colorColumns[i];
+    ctx.fillRect(
+      CLOUD_X + TEXT_WIDTH + (TEXT_WIDTH + TEXT_WIDTH) * i, 
+      CLOUD_X - GAP,
+      BAR_WIDTH,
+      (BAR_HEIGHT * times[i]) / maxTime
+    );
+  }
 };
